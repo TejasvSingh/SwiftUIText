d096dc1f32444a56cf2d35a927c8807c59eabe86
TejasvSingh <singhtejasv98@gmail.com>
2025-10-02 10:18:27 -0500

    Initial Commit

---
+++ SwiftUIText/SwiftUITextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextApp.swift
+//  SwiftUIText
+//
+//  Created by Tejasv Singh on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
