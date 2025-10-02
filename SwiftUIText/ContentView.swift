59c886da7ee0550c6e86df3fb42547ac43d96977
TejasvSingh <singhtejasv98@gmail.com>
2025-10-02 10:50:44 -0500

    Initial Commit

--- SwiftUIText/ContentView.swift
+++ SwiftUIText/ContentView.swift
@@ -10,10 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world!").font(.title)
+            Text("Hello, world!").font(.caption)
+            Text("Hello, world!").font(.body)
+            Text("Hello, world!").fontWeight(.heavy)
+            Text("Hello, world!").bold()
+            Text("Hello, world!").underline().italic()
+            Text("Hello, world!").strikethrough()
+            Text("Hello, world!").underline(true, color: Color.blue)
+            Text("Hello, world!").strikethrough(true, color: Color.green)
+            Text("Hello, world!").font(.system(size: 20, weight: .semibold, design: .serif))
+            Text("Hello, world! This is a swiftui course and I am really happy to be learning SwiftUI").baselineOffset(20).multilineTextAlignment(.leading)
+            Text("Hello, world! This is a swiftui course and I am really happy to be learning SwiftUI").kerning(5) .multilineTextAlignment(.leading)
+            Text("Hello, world! This is a swiftui course and I am really happy to be learning SwiftUI").foregroundColor(.red)
+                .multilineTextAlignment(.leading).frame(width: 100, height: 100, alignment: .center).minimumScaleFactor(0.5)
+            Text("Hello World!").multilineTextAlignment(.leading).frame(alignment: .leading)
+            Text("hello world".capitalized)
+            Text("hello world".uppercased())
+            Text("HELLO WORLD".lowercased())
         }
         .padding()
     }
